58ebc169d62db880531d087d1043532084032107
lorgar-avrelian <victor-14-244@mail.ru>
2025-09-23 20:23:04 +0300

    In progress

--- src/main/java/lorgar/avrelian/Main.java
+++ src/main/java/lorgar/avrelian/Main.java
@@ -1,7 +1,12 @@
 package lorgar.avrelian;
 
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main {
@@ -9,15 +14,28 @@ public class Main {
 
     public static void main(String[] args) {
         final String dumpPath = "D:" + File.separator + DUMP_NAME;
+        final Path currentPath = Paths.get("").toAbsolutePath();
         try {
+            final String newDumpName = DUMP_NAME.substring(0, DUMP_NAME.lastIndexOf("-")) + ".sql";
+            final Path resultPath = Path.of(currentPath + File.separator + "result" + File.separator + newDumpName);
+            if (!Files.exists(resultPath.getParent())) {
+                Files.createDirectory(resultPath.getParent());
+            }
+            Files.deleteIfExists(resultPath);
+            final File result = new File(resultPath.toUri());
+            final BufferedWriter writer = new BufferedWriter(new FileWriter(result));
             final File dump = new File(dumpPath);
             final Scanner scanner = new Scanner(dump);
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                System.out.println(line);
+                if (!line.startsWith("/*") && !line.startsWith("--")) {
+                    writer.write(line);
+                    writer.newLine();
+                }
             }
             scanner.close();
-        } catch (FileNotFoundException e) {
+            writer.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
