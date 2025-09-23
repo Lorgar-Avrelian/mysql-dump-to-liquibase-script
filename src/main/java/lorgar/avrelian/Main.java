426a522c8b53bf3f29a8acc8d3e2eec1134f5073
lorgar-avrelian <victor-14-244@mail.ru>
2025-09-23 19:53:19 +0300

    In progress

--- src/main/java/lorgar/avrelian/Main.java
+++ src/main/java/lorgar/avrelian/Main.java
@@ -1,7 +1,24 @@
 package lorgar.avrelian;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
+    private static final String DUMP_NAME = "dump-db_msp-202509231039.sql";
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        final String dumpPath = "D:" + File.separator + DUMP_NAME;
+        try {
+            final File dump = new File(dumpPath);
+            final Scanner scanner = new Scanner(dump);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
